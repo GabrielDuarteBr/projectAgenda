a2c07f3caef551c9f365027b9d93c8e2f820ee2f
gabriel <gabrielduartedesilveira@gmail.com>
2024-09-16 21:21:21 -0300

    Corrigido o adicionar linhas no JS

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
 const form = document.getElementById("formAgenda");
 const nomes = [];
 const fones = [];
-
+    
 linhas = '';
 
 function adicionarLinha() {
     let insertName = document.getElementById("insertName");
     let insertFone = document.getElementById("insertFone");
 
-    nomes.push("insertName.value");
-    fones.push("insertFone.value");
-
-    let linha = "<tr>";
-    linha += `<td>${insertName.value}</td>`;
-    linha += `<td>${insertFone.value}</td>`;
-    linha += `</tr>`;
-
-    linhas += linha
-};
-
-function calcularTotalContatos() {
-
-};
-
-function adionarQuantidadeContatos() {
-
+        nomes.push("insertName.value");
+        fones.push("insertFone.value");
+    
+        let linha = "<tr>";
+        linha += `<td>${insertName.value}</td>`;
+        linha += `<td>${insertFone.value}</td>`;
+        linha += `</tr>`;
+    
+        linhas += linha;
+    
+        document.querySelector("tbody").innerHTML = linhas;
+    
+        insertName.value = "";
+        insertFone.value = "";
 }
-
-
 
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     adicionarLinha();
-    calcularTotalContatos();
-    adionarQuantidadeContatos();
 });
